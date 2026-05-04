9c2496c8eb634ef834ae46735c60838df5bb5bf8
JesseBA04 <jesse.berndtalonso@gmail.com>
2026-05-04 14:12:13 -0500

    added the ability for the connection to neon to close

--- database.py
+++ database.py
@@ -8,10 +8,16 @@ with open("pw.txt", "r") as f:
 # connect to the database
 conn = psycopg.connect(f"postgresql://neondb_owner:{password}@ep-spring-wind-anixzc55-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
-# route to files which hold different information
-choice = input("Which table: ")
-operation = input("Which operation: ")
+while True:
+    # route to files which hold different information
+    choice = input("Which table (or 'quit' to exit): ")
+    if choice == "quit":
+        break
+    operation = input("Which operation: ")
 
 
-fileToRun = importlib.import_module(f"routes." + operation + "." + choice)
-fileToRun.run(conn)
+    fileToRun = importlib.import_module(f"routes." + operation + "." + choice)
+    fileToRun.run(conn)
+
+# This will close the connection after the user has inputted all the data they wanted to
+conn.close()
